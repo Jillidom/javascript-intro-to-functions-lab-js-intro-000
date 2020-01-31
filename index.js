849deedc8d93aa1c16084613ad4afd5fbf5d27df
Jillian Domingue <jmdomingue@gmail.com>
2020-01-31 04:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ function logWhisper(string){
    if(string.toLowerCase() === string){
      return "I can't hear you!";
    }
+   if(string.toUpperCase() === string){
+     return "I can't hear you!";
+   }
+   if(string === "I love you, Grandma."){
+     return "I love you, too.";
+   }
+   return "Are you eating enough?";
  }
 
  
